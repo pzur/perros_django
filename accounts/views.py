511d89aaf6e865008cb029aa5547992df1c6baee
luis <luic.sistem@gmail.com>
2020-02-05 14:01:30 -0500

    register second user

--- accounts/views.py
+++ accounts/views.py
@@ -10,6 +10,15 @@ from .models import *
 
 def registerPage(request):
     form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+
+
+
     context = {'form':form}
     return render(request, 'accounts/register.html', context)
 
