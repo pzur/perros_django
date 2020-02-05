3da702c19c476d0a739d36fbe998c65654671129
luis <luic.sistem@gmail.com>
2020-02-05 16:08:06 -0500

    insertando messages

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
 
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib import messages
+
 from .models import *
 from .forms import  OrderForm, CreateUserForm
 # from .filters import OrderFilter
@@ -15,6 +19,9 @@ def registerPage(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'la cuenta ya fue creada' + user)
+            return redirect('login')
 
 
 
@@ -23,10 +30,26 @@ def registerPage(request):
     return render(request, 'accounts/register.html', context)
 
 def loginPage(request):
-    context = {}
-    return render(request, 'accounts/login.html', context)
+
+     if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username= username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'username or password is incorrect')
+            
+             
+
+     context = {}
+     return render(request, 'accounts/login.html', context)
 
 def home(request):
+
     return render(request, 'accounts/dashboard.html')
 
 def product(request):
